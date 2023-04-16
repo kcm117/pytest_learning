361b9507fee4faf19fdffd538a1609c2ee880de4
KC Munnings <kc@kcmunnings.com>
2023-04-16 16:06:15 -0700

    update env script

--- create_env.sh
+++ create_env.sh
@@ -1,5 +1,8 @@
+# Ensure VENV is installed
 sudo apt install python3-venv python3-pip
+# Create Python virtual environment
 python3 -m venv .venv
 source .venv/bin/activate
 python3 -m pip install --upgrade pip
+echo Virtual Environment Created
 python3 -m pip install -r requirements.txt
